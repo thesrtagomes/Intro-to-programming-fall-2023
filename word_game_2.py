017383046aef9cc7544578d68a75da1c986f0099
thesrtagomes <pamela.gomes.work@gmail.com>
2023-10-28 14:47:21 -0300

    adding new activities

---
+++ word_game_2.py
@@ -0,0 +1,17 @@
+print('Please enter the following:')
+print('')
+
+adjective = input(str('Adjective: '))
+animal = input(str('Animal: '))
+verb_1 = input(str('Verb: '))
+exclamation = input(str('Exclamation: '))
+verb_2 = input(str('Verb: '))
+verb_3 = input(str('Verb: '))
+print('')
+print('Your story is:')
+print('')
+
+print(f'The other day, I was really in trouble. It all started when I saw a '
+     f'very {adjective} {animal} {verb_1} down the hallway. "{exclamation}!!" I yelled. '
+      f'But all I could think to do was to {verb_2} over and over again . Miraculously, that coused it to stop, '
+      f'but not before I tried to {verb_3} right in front of my family.')
